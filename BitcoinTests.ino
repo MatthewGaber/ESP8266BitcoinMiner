4da753f308c42d71a9d047c31ff8dda8c144392b
MerlotMachina <33920307+MerlotMachina@users.noreply.github.com>
2017-12-21 16:50:05 +0700

    Update BitcoinTests.ino

--- BitcoinTests.ino
+++ BitcoinTests.ino
@@ -11,16 +11,12 @@ uint8_t hashbytes[80];
 
 uint8_t* hex_decode(const char *in, size_t len, uint8_t *out)
 {
-        unsigned int i, t, hn, ln;
-
-        for (t = 0,i = 0; i < len; i+=2,++t) {
-
-                hn = in[i] > '9' ? in[i] - 'a' + 10 : in[i] - '0';
-                ln = in[i+1] > '9' ? in[i+1] - 'a' + 10 : in[i+1] - '0';
-
-                out[t] = (hn << 4 ) | ln;
+        unsigned int i, mg, ng, rg;
+        for (mg = 0, i = 0; i < len; i+=2,++mg) {
+                ng = in[i] > '9' ? in[i] - 'a' + 10 : in[i] - '0';
+                rg = in[i+1] > '9' ? in[i+1] - 'a' + 10 : in[i+1] - '0';
+                out[mg] = (ng << 4 ) | rg;
         }
-
         return out;
 }
 
@@ -40,16 +36,14 @@ void hash(){
   unsigned long delta = ended - start;
   Serial.println(delta);
   Serial.print("Big Endian: ");
-  for (byte i=32; i > 0; i--)
-   {
+  for (byte i=32; i > 0; i--){
      if (hash2[i-1]<0x10) { Serial.print('0'); }
       Serial.print(hash2[i-1], HEX);
    }
    
    Serial.println();
    Serial.print("Little Endian: ");
-   for (byte i=0; i < SHA256_SIZE ; i++)
-   {
+   for (byte i=0; i < SHA256_SIZE ; i++){
      if (hash2[i]<0x10) { Serial.print('0'); }
       Serial.print(hash2[i], HEX);
    }
